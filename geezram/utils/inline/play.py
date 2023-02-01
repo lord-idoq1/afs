da3958faa41a44f918250785dc7580854e5b9386
OnlyMeRiz <73426077+Onlymeriz@users.noreply.github.com>
2023-02-02 04:16:39 +0700

    Update play.py

--- geezram/utils/inline/play.py
+++ geezram/utils/inline/play.py
@@ -14,25 +14,25 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     geez = math.floor(percentage)
     if 0 < geez <= 10:
-        bar = "▮▯▯▯▯▯▯▯▯▯"
+        bar = "◉○○○○○○○○○"
     elif 10 < geez < 20:
-        bar = "▮▮▯▯▯▯▯▯▯▯"
+        bar = "◉◉○○○○○○○○"
     elif 20 <= geez < 30:
-        bar = "▮▮▮▯▯▯▯▯▯▯"
+        bar = "◉◉◉○○○○○○○"
     elif 30 <= geez < 40:
-        bar = "▮▮▮▮▯▯▯▯▯▯"
+        bar = "◉◉◉◉○○○○○○"
     elif 40 <= geez < 50:
-        bar = "▮▮▮▮▮▯▯▯▯▯"
+        bar = "◉◉◉◉◉○○○○○"
     elif 50 <= geez < 60:
-        bar = "▮▮▮▮▮▮▯▯▯▯"
+        bar = "◉◉◉◉◉◉○○○○"
     elif 60 <= geez < 70:
-        bar = "▮▮▮▮▮▮▮▯▯▯"
+        bar = "◉◉◉◉◉◉◉○○○"
     elif 70 <= geez < 80:
-        bar = "▮▮▮▮▮▮▮▮▯▯"
+        bar = "◉◉◉◉◉◉◉◉○○"
     elif 80 <= geez < 95:
-        bar = "▮▮▮▮▮▮▮▮▮▯"
+        bar = "◉◉◉◉◉◉◉◉◉○"
     else:
-        bar = "▮▮▮▮▮▮▮▮▮▮"
+        bar = "◉◉◉◉◉◉◉◉◉◉"
 
     buttons = [
         [
@@ -43,17 +43,17 @@ def stream_markup_timer(_, videoid, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="‣",
+                text="⏯",
                 callback_data=f"ADMIN Resume|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="◉", callback_data=f"ADMIN Pause|{chat_id}"
+                text="◼", callback_data=f"ADMIN Pause|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+                text="⏭", callback_data=f"ADMIN Skip|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="⨯", callback_data=f"ADMIN Stop|{chat_id}"
+                text="✖", callback_data=f"ADMIN Stop|{chat_id}"
             ),
         ],
         [
@@ -80,25 +80,25 @@ def telegram_markup_timer(_, chat_id, played, dur):
     percentage = (played_sec / duration_sec) * 100
     geez = math.floor(percentage)
     if 0 < geez <= 10:
-        bar = "▮▯▯▯▯▯▯▯▯▯"
+        bar = "◉○○○○○○○○○"
     elif 10 < geez < 20:
-        bar = "▮▮▯▯▯▯▯▯▯▯"
+        bar = "◉◉○○○○○○○○"
     elif 20 <= geez < 30:
-        bar = "▮▮▮▯▯▯▯▯▯▯"
+        bar = "◉◉◉○○○○○○○"
     elif 30 <= geez < 40:
-        bar = "▮▮▮▮▯▯▯▯▯▯"
+        bar = "◉◉◉◉○○○○○○"
     elif 40 <= geez < 50:
-        bar = "▮▮▮▮▮▯▯▯▯▯"
+        bar = "◉◉◉◉◉○○○○○"
     elif 50 <= geez < 60:
-        bar = "▮▮▮▮▮▮▯▯▯▯"
+        bar = "◉◉◉◉◉◉○○○○"
     elif 60 <= geez < 70:
-        bar = "▮▮▮▮▮▮▮▯▯▯"
+        bar = "◉◉◉◉◉◉◉○○○"
     elif 70 <= geez < 80:
-        bar = "▮▮▮▮▮▮▮▮▯▯"
+        bar = "◉◉◉◉◉◉◉◉○○"
     elif 80 <= geez < 95:
-        bar = "▮▮▮▮▮▮▮▮▮▯"
+        bar = "◉◉◉◉◉◉◉◉◉○"
     else:
-        bar = "▮▮▮▮▮▮▮▮▮▮"
+        bar = "◉◉◉◉◉◉◉◉◉◉"
 
     buttons = [
         [
@@ -109,17 +109,17 @@ def telegram_markup_timer(_, chat_id, played, dur):
         ],
         [
             InlineKeyboardButton(
-                text="‣",
+                text="⏯",
                 callback_data=f"ADMIN Resume|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="◉", callback_data=f"ADMIN Pause|{chat_id}"
+                text="◼", callback_data=f"ADMIN Pause|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+                text="⏭", callback_data=f"ADMIN Skip|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="⨯", callback_data=f"ADMIN Stop|{chat_id}"
+                text="✖", callback_data=f"ADMIN Stop|{chat_id}"
             ),
         ],
         [
@@ -301,17 +301,17 @@ def panel_markup_1(_, videoid, chat_id):
     buttons = [
         [
             InlineKeyboardButton(
-                text="‣",
+                text="⏯",
                 callback_data=f"ADMIN Resume|{chat_id}",
             ),
             InlineKeyboardButton(
-                text="◉", callback_data=f"ADMIN Pause|{chat_id}"
+                text="◼", callback_data=f"ADMIN Pause|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="‣‣I", callback_data=f"ADMIN Skip|{chat_id}"
+                text="⏭", callback_data=f"ADMIN Skip|{chat_id}"
             ),
             InlineKeyboardButton(
-                text="⨯", callback_data=f"ADMIN Stop|{chat_id}"
+                text="✖", callback_data=f"ADMIN Stop|{chat_id}"
             ),
         ],
         [
